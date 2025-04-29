049e698f5211c72f7f6fc202e7232971403128dd
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-04-29 08:12:28 -0300

    ...

--- Front/src/Componentes/Card/index.jsx
+++ Front/src/Componentes/Card/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './style.sass'
 
 export function Card({ dado }) {
     return(
         <div className="card_container">
             <h1>{dado.nome}</h1>
+            <img src={dado.img} />
         </div>
     )
 }
--- Front/src/Componentes/ListAnimais/index.jsx
+++ Front/src/Componentes/ListAnimais/index.jsx
@@ -5,20 +5,27 @@ import api from '../../Service/api';
 export function ListAnimais(){
     const [animaisList, setAnimaisList] = useState([]);
 
-    useEffect(() => {
-        const infoAPI = async () => {
-            let infoAnimal = await api.get_infosAnimais();
+    // useEffect(() => {
+    //     const infoAPI = async () => {
+    //         let infoAnimal = await api.get_infosAnimais();
 
-            setAnimaisList(infoAnimal);
-            console.log(infoAnimal)
+    //         setAnimaisList(infoAnimal);
+    //         console.log(infoAnimal)
+    //     }
+
+    //     infoAPI();
+    // })
+
+    const dado = [
+        {
+            "nome": "Ariane",
+            "img": "https://thaka.bing.com/th/id/OIP.EU4rGtqFQAtuj4azJqkkvQHaFD?w=302&h=206&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2"
         }
-
-        infoAPI();
-    })
+    ]
 
     return(
         <div>
-            {animaisList.map((dado, key) => (
+            {dado.map((dado, key) => (
                 <Card key={key} dado={dado}/>
             ))}
         </div>
